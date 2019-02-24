87367ea4816c724a31dfda78dce292e97244f4e8
simplepeng <383559698@qq.com>
2019-02-24 20:50:18 +0800

    bug:release add res

--- spiderman-no-op/src/main/java/com/simple/spiderman/SpiderMan.java
+++ spiderman-no-op/src/main/java/com/simple/spiderman/SpiderMan.java
@@ -2,14 +2,19 @@ package com.simple.spiderman;
 
 import android.content.Context;
 
-public class SpiderMan{
+public class SpiderMan {
 
     public static final String TAG = "SpiderMan";
+    private static SpiderMan spiderMan = new SpiderMan();
 
     private SpiderMan() {
     }
 
-    public static void init(Context context) {
+    public static SpiderMan init(Context context) {
+        return spiderMan;
+    }
+
+    public void setTheme(int themeId) {
 
     }
 
