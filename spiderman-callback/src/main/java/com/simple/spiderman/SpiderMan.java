ba5ed7ac76ec0048bceb614cc09954fa302c695d
chenpeng <383559698@qq.com>
2021-09-22 14:55:46 +0800

    转发ExceptionHandler

--- spiderman-callback/src/main/java/com/simple/spiderman/SpiderMan.java
+++ spiderman-callback/src/main/java/com/simple/spiderman/SpiderMan.java
@@ -17,7 +17,12 @@ public class SpiderMan implements Thread.UncaughtExceptionHandler {
 
     private static OnCrashListener mOnCrashListener;
 
+    private static Thread.UncaughtExceptionHandler mAlreadyExistedExceptionHandler;
+
     private SpiderMan() {
+        if (Thread.getDefaultUncaughtExceptionHandler() != null && Thread.getDefaultUncaughtExceptionHandler() != this) {
+            mAlreadyExistedExceptionHandler = Thread.getDefaultUncaughtExceptionHandler();
+        }
         Thread.setDefaultUncaughtExceptionHandler(this);
     }
 
@@ -29,6 +34,7 @@ public class SpiderMan implements Thread.UncaughtExceptionHandler {
     @Override
     public void uncaughtException(Thread t, Throwable ex) {
         callbackCrash(t, ex);
+        transmitException(t, ex);
         SpiderManUtils.killApp();
     }
 
@@ -58,5 +64,11 @@ public class SpiderMan implements Thread.UncaughtExceptionHandler {
     private static void callbackCrash(Thread t, Throwable ex) {
         if (mOnCrashListener == null) return;
         mOnCrashListener.onCrash(t, ex);
+    }
+
+    private void transmitException(Thread t, Throwable ex) {
+        if (mAlreadyExistedExceptionHandler == null) return;
+        if (mAlreadyExistedExceptionHandler == this) return;
+        mAlreadyExistedExceptionHandler.uncaughtException(t, ex);
     }
 }
